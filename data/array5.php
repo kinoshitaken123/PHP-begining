a1ad82f9fda18e3301cdfa196158e2204d2a9494
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-10-13 18:59:36 +0900

    変数の値を出力

---
+++ data/array5.php
@@ -0,0 +1,10 @@
+<body>
+  <?php
+  $userData = [12345, 'Tarou Yamada', 'Tokyo', 32];
+  list($id, $name, $prefecture, $age) = $userdata;
+  ?>
+  <p>ユーザーID:<?= $id ?></p>
+  <p>ユーザー名:<?= $name ?></p>
+  <p>都道府県:<?= $id ?></p>
+  <p>ユーザー名:<?= $name ?></p>
+</body>
