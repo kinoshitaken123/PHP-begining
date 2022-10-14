9d7b24fad7a80a2dbdd190eae2a57124de981a86
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-10-15 06:39:16 +0900

    マジック定数

---
+++ data/constant2.php
@@ -0,0 +1,12 @@
+<body>
+  <?php
+  function someFunction()
+  {
+    echo '現在の関数名は:', __FUNCTION__, 'です。<br>';
+  }
+  echo '現在のファイル名は:', __FILE__, 'です。<br>';
+  echo '現在の行番号は:', __LINE__, 'です。<br>';
+  echo '現在のディレクトリは:', __DIR__, 'です。<br>';
+  someFunction();
+  ?>
+</body>
