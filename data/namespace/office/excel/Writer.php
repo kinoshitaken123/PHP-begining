d685165228e5f63b8bc1795ae20c83f7d9c91399
kinoshitaken123 <k1k7s9e3@gmail.com>
2022-11-26 23:25:23 +0900

    feature 名前を空間を定義する

---
+++ data/namespace/office/excel/Writer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Office\Excel;
+
+class Writer
+{
+  public function write(): void
+  {
+    echo 'Excelを出力します。', PHP_EOL;
+  }
+}
---
+++ data/namespace/office/word/Writer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Office\Word;
+
+class Writer
+{
+  public function write(): void
+  {
+    echo 'Wordを出力します。', PHP_EOL;
+  }
+}
