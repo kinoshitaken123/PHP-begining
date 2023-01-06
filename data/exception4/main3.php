9c17ee35743f18f8f1aa17f46cfa29ef392755c0
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-06 23:25:00 +0900

    例外の種類に応じた処理　catchブロックをつなげる

--- data/exception4/main3.php
+++ data/exception4/main3.php
@@ -1,10 +1,11 @@
+<?php
 $model = new UserModel();
 try{
   $profile =$model->findProfile(1001);
 }catch(Exception$$exception){
   echo file_get_contents('error-unknown.html');
   return;
-}catch(PageNotFoundException$$exception){
+}catch(PageNotFoundException$exception){
   echo file_get_contents('error-page-not-found.html');
   return;
 }
