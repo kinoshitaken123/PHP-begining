e6a8041935efa6073a52fc10bf337a549300163a
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-19 23:20:14 +0900

    正規表現　半角数字・半角数字およびハイフンのみかチェック

--- data/reg/match.php
+++ data/reg/match.php
@@ -11,5 +11,19 @@
     // [0-9]{3} 0~9の連続する3文字
     // 123-4567
     print_r($matched);
+
+    echo '文字列から16進数表記のカラーコードを見つける', PHP_EOL;
+    $string = 'darkviolet - 濃いすみれ色 - #9400d3a';
+    preg_match('/#[0-9a-f]{6}/ui', $string, $matched, PREG_OFFSET_CAPTURE);
+    // 9400d3aとマッチ
+    print_r($matched);
+
+    echo '半角英数字のみを入力チェックする', PHP_EOL;
+    $input = '0-3-９９９９-0000';
+    if (preg_match('/\A[0-9\-]+\z/u/', $input)){
+      echo '入力に問題ありません';
+    } else {
+      echo '半角文字とハイフンのみで入力してください';
+    }
   ?>
 </body>
