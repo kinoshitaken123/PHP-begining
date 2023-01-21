902f30c569580dd7a509f6e0925541f95f534718
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-21 22:55:47 +0900

    半角英数字のみを入力チェックする

--- data/reg/match.php
+++ data/reg/match.php
@@ -33,5 +33,14 @@
     } else {
       echo '半角数字のみで入力してください', PHP_EOL;
     }
+
+    echo '半角英数字のみを入力チェックする(i修飾子)', PHP_EOL;
+    $input = 'Hello';
+    if (preg_match('/\A[a-z0-9]+\z/ui', $input)) {
+      echo '入力に問題ありません', PHP_EOL;
+    } else {
+      echo '半角英数字のみで入力してください', PHP_EOL;
+    }
+    
   ?>
 </body>
