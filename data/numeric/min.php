a95fe0ba67692fad9dbcc104942b069b9cc9a193
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-23 22:57:44 +0900

    最小値/最大値を得る

---
+++ data/numeric/min.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1); ?>
+<body>
+  <?php  
+    $numbers = [80, 24, 100, 34, 62];
+    echo '最小値:', min($numbers), PHP_EOL;
+    echo '最大値:', max($numbers), PHP_EOL;
+  ?>
+</body>
