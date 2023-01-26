894760620a2692a4d6abd8eab2a7644d63eb156f
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-26 23:53:27 +0900

    日付/時間を操作する

--- data/date/basic.php
+++ data/date/basic.php
@@ -10,7 +10,17 @@
       // インスタンスを変数として保存する必要がなければ、以下のように1ステップでも出力する
       echo (new DateTime('2019-02-25 17:12:34'))->format('Y.m.d H:i:s'), PHP_EOL;
 
+      //現在日時を元にインスタンスを作る
       $dateTime = new DateTime();
+      echo $dateTime->format('Y.m.d H:i:s'), PHP_EOL;
+
+      // 現在日時の3時間後のインスタンスを作る
+      $dateTime = new DateTime('+3 hours');
+      echo $dateTime->format('Y.m.d H:i:s'), PHP_EOL;
+
+      // 現在日時でインスタンスを作った後、年月日のみを上書き指定する
+      $dateTime = newDateTime();
+      $dateTime->setDate(2019,2,25);
       echo $dateTime->format('Y.m.d H:i:s'), PHP_EOL;
   ?>
 </body>
