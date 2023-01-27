298c1348efea8b08ba5ca974951277ee7ad0d9cc
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-27 23:45:03 +0900

    日付/時間を操作する

--- data/date/basic.php
+++ data/date/basic.php
@@ -22,5 +22,22 @@
       $dateTime = newDateTime();
       $dateTime->setDate(2019,2,25);
       echo $dateTime->format('Y.m.d H:i:s'), PHP_EOL;
+
+      // 現在日時でインスタンスを作った後、年月日と時分秒を上書き指定する
+      $dataTime = new DateTime();
+      $dataTime->setDate(2019, 2 , 25)->setTime(17, 12, 34);
+      echo $dataTime->format('Y.m.d H:i:s'). PHP_EOL;
+
+      // 現在日時でインスタンス作成後、年月日と時分秒を上書きを指定する
+      echo (new DateTime('2019-02-25 17:12:34'))
+          ->modify('+5 days')
+          ->modify('+12 hours')
+          ->modify('Y.m.d H:i:s'), PHP_EOL;
+
+      // 2019-2-25 17:12:34のインスタンスを作った後に、次の土曜日まで進め、時刻を00:00:00にするs
+      echo (new DateTime('2019-02-25 17:12:34'))
+          ->modify('Next Saturday')
+          ->setTime(0, 0, 0)
+          ->format('Y.m.d H:i:s'), PHP_EOL;
   ?>
 </body>
