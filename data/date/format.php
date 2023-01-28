392014887dab450f87ee6b985918eb69ad1311da
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-28 23:52:38 +0900

    日付をフォーマットする -DataTime:format

---
+++ data/date/format.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1); ?>
+  <body>
+    <?php   
+      date_default_timezone_set('Asia/Tokyo');
+      $date = new DateTime('2019-02-15 17:12:34');
+
+      echo $date->format('Y.m.d H:i:s'), PHP_EOL;
+      echo $date->format('Y.m.d H:i'), PHP_EOL;
+      echo $date->format('Y年m月d日(D) H時i分'), PHP_EOL;
+      // その月の日数
+      echo $date->format('Y.m.t'), PHP_EOL;
+    ?>
+  </body>
