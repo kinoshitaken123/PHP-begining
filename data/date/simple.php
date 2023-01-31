fa30ae596a69761f4bd2a86d5f1df8fc534b41f9
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-01-31 23:35:50 +0900

    ちょっとした日時操作のための手軽に使える内部関数

---
+++ data/date/simple.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1); ?>
+<body>
+  <?php  
+      echo '現在のUnixタイム:', PHP_EOL;
+      echo time(), PHP_EOL;
+
+      echo '現在日時を出力:', PHP_EOL;
+      echo date('Y-m-d H:i:s'), PHP_EOL;
+  ?>
+</body>
