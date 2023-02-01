b95d022e8df01b45862d6f77ed263b827e5fa317
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-02-01 23:57:56 +0900

    ちょっとした日時操作のための手軽に使える内部関数

--- data/date/simple.php
+++ data/date/simple.php
@@ -6,5 +6,14 @@
 
       echo '現在日時を出力:', PHP_EOL;
       echo date('Y-m-d H:i:s'), PHP_EOL;
+
+      echo '現在の3日後の日時を出力(time使用):', PHP_EOL;
+      echo date('Y-m-d H:i:s', time() + 60 * 60 *24 * 30), PHP_EOL;
+
+      echo '現在の3日後の日時を出力(strtotime使用):', PHP_EOL;
+      echo date('Y-m-d H:i:s', strtotime('+3 days')), PHP_EOL;
+
+      echo '2019-01-10 10:00:00の3時間前を出力', PHP_EOL;
+      echo date('Y-m-d H:i:s', strtotime('-3 hours', strtotime('2019-01-10 10:00:00'))), PHP_EOL;
   ?>
 </body>
