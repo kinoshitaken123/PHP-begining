71c85743db222cc6223e4a3582debb13829c47ca
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-02-03 23:43:11 +0900

    配列の要素を追加/削除する

--- data/array/elements.php
+++ data/array/elements.php
@@ -21,5 +21,18 @@
       echo '●配列の先頭の要素を取り出す', PHP_EOL;
       $headElement = array_shift($chars);
       print_r($chars);
+
+      echo '●配列の末尾の要素を取り出す', PHP_EOL;
+      $lastElement = array_pop($chars);
+      print_r($chars);
+
+      echo '●配列のキー番号2から5要素分,切り出す', PHP_EOL;
+      $lastElement = array_slice($chars, 2,5);
+      print_r($chars);
+
+      
+      echo '●配列のキー番号2から5要素を削除する', PHP_EOL;
+      unset($chars[2]);
+      print_r($chars);
   ?>
 </body>
