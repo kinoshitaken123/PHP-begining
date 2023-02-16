e4c8376be63a421dad55dbeaa7e8c645d3347f3b
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-02-16 23:55:27 +0900

    配列をソートする

--- data/array/sort.php
+++ data/array/sort.php
@@ -14,5 +14,22 @@
     echo '配列値を文字列として昇順ソートする', PHP_EOL;
     sort($prices, SORT_STRING);
     print_r($prices);
+
+    $files = ['imagel.png', 'image105.png', 'image2.png', 'image3.png', 'image44.png'];
+    echo '配列値で自然順ソートする', PHP_EOL;
+    sort($files, SORT_NATURAL);
+    print_r($files);
+
+    $items = [
+      100001 => '掃除機',
+      100002 => 'エアコン',
+      100003 => 'PC',
+      100004 => 'テレビ',
+    ];
+
+    echo '連想配列キーの降順でソートする', PHP_EOL;
+    ksort($items);
+    print_r($items);
+
   ?>
 </body>
