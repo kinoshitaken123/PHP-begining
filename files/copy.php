60d915d3cd1fcc9580e42e590d92ef4d1486d201
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-03-13 23:50:51 +0900

    ファイルをコピー

---
+++ files/copy.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1); ?>
+<body>
+  <?php  
+      echo '●flower.phpをコピーしてflower-copy.pngを作ります。', PHP_EOL;
+      copy('./files/flower.png', './files/flower-copy1.png');
+  ?>
+</body>
