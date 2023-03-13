a32052438d410593a3e8bf6aca5decb9e8f0c863
kinoshitaken123 <k1k7s9e3@gmail.com>
2023-03-13 23:57:36 +0900

    ファイルをコピー/移動/削除する

--- files/copy.php
+++ files/copy.php
@@ -3,5 +3,11 @@
   <?php  
       echo '●flower.phpをコピーしてflower-copy.pngを作ります。', PHP_EOL;
       copy('./files/flower.png', './files/flower-copy1.png');
+
+      echo '●flower.phpをコピーしてflower-copy.pngをリネームします。', PHP_EOL;
+      rename('./files/flower-copy1.png', './files/flower-rename1.png');
+
+      echo '●flower.phpをコピーしてflower-copy.pngを作ります。', PHP_EOL;
+      unlink('./files/flower-rename1.png');
   ?>
 </body>
